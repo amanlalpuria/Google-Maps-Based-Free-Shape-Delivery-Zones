0af45d926dd6ad48802d7535b43b1c986d117262
Aman Lalpuria <aman.sharma163@gmail.com>
2025-05-20 23:08:35 +0530

    Setup

---
+++ src/main/java/com/free/map/DeliveryZonesApplication.java
@@ -0,0 +1,13 @@
+package com.free.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryZonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryZonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/free/map/DeliveryZonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryZonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
